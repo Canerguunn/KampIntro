957c30be353545bbd876adf21f5d7ea2439e57b0
Can <Can@CAN>
2021-02-26 18:35:01 +0300

    asdf

---
+++ Metotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Armut";
+
+            Product[] Products = new Product[] {urun1,urun2 };
+
+            foreach (Product urun in Products)
+            {
+                Console.WriteLine(urun.Adi);
+            }
+
+            SepetManager seperManager = new SepetManager();
+            seperManager.Ekle(urun1);
+            seperManager.Ekle(urun2);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Product product) {
+            Console.WriteLine("sepete eklendi" + product.Adi);
+        }
+    }
+}
