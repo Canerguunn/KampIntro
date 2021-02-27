41844d7762ef99f25373eca930342559f7e9f600
Can <Can@CAN>
2021-02-27 15:39:51 +0300

    27.02.2021

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OOP1;
 
 namespace OOP
 {
@@ -7,7 +8,15 @@ namespace OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("dadsd");
+            Product pr1 = new Product();
+            pr1.CategoryId = 1;
+            pr1.Id = 1;
+            pr1.ProductName = "Sehpa";
+            pr1.UnitPrice = 2;
+            pr1.UnitsInStock = 3;
+
+            ProductManager pm = new ProductManager();
+            pm.Add(pr1);
         }
     }
 }
