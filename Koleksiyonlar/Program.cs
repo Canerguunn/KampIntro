b65b07e4d4732bc7f1c700c80722491a19c8a7ba
Can <Can@CAN>
2021-02-27 17:52:51 +0300

    New

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Sakir", "CAn", "Ergun", "Ds" };
+            isimler.Add("Can");
+
+        }
+    }
+}
