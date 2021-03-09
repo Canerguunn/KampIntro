e92ef324ec201434b267b8391e7e7a85cc715780
Can <Can@CAN>
2021-03-09 20:47:55 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Business Classses
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) { 
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKredisiManager = new IhtiyacKredisiManager();
+            IKrediManager tasitKredisiManager = new TasitKredisiManager();
+            IKrediManager konutKredisiManager = new KonutKredisiManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { databaseLoggerService, fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKredisiManager, loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKredisiManager,konutKredisiManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
